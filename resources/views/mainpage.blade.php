553db82def15a40801f689b1523f1ba8945e8111
jeANSdv <bazuev.4@yandex.ru>
2024-06-23 16:05:04 +0400

    mainpage

--- resources/views/mainpage.blade.php
+++ resources/views/mainpage.blade.php
@@ -8,17 +8,19 @@
     <div class="container-fluid card border-0 p-0">
         <img src="/img/mainpage/gym.jpg" class="img-fluid card-img rounded-0" alt="">
         <div class="card-img-overlay text-white d-flex flex-column justify-content-center align-items-center">
-            <h1 class="card-title">ТРЕНАЖЕРНЫЙ ЗАЛ APEX FITNESS</h1>
-            <h6 class="card-text text-break text-center">С другой стороны постоянное
-                информационно-пропагандистское
-                обеспечение
-                нашей
-                деятельветствует насущным
-                потребностям С другой стороны постоянное информационно-пропагандистское обеспечение нашей деятельности
-                представляет собой интересный эксперимент проверки систем массового участия. Разнообразный и богатый
-                опыт рамки и место обучения кадров способствует подготовке и реализации направлени в формировании
-                системыб</h6>
-            <button>О НАС</button>
+            <h1 class="card-title text-center">ТРЕНАЖЕРНЫЙ ЗАЛ APEX FITNESS</h1>
+            <p
+                class="card-text text-break text-center fs-5 col-8 shadow-sm d-none d-sm-none d-md-block d-lg-block d-xl-block d-xxl-block">
+                Lorem ipsum dolor sit amet,
+                consectetur
+                adipiscing
+                elit,
+                sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            <a class="col-6 col-sm-6 col-lg-2 btn cta-b rounded-3 py-2" href="#">О НАС</a>
         </div>
     </div>
 </div>
